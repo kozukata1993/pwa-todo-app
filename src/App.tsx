6af1415aa9d71f1f66d874bc680e9c0eb3aa8704
kento <kozukata1993@gmail.com>
2020-09-09 16:41:02 +0900

    eslint, prettier, huskyをインストール

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  console.log('hello');
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +23,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
