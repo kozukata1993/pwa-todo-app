a2745831fe305c8f8ed9ea8aa91605fb65807840
kento <kozukata1993@gmail.com>
2020-09-10 21:43:18 +0900

    firestore, functions, github actions

---
+++ src/firebase/index.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/firebase-storage';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: 'todo-app-4850f',
+  appID: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(config);
+export const { auth, firestore, functions, storage } = firebase;
